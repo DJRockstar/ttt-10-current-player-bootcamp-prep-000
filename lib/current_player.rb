65feddbf1b226e3503c0af4265f031707ec2a1b6
DJ Jaiswal <digvijays18@gmail.com>
2018-03-06 22:34:41 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,7 +9,11 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board)==0
+  if turn_count(board)==0 && turn_count(board)<10
     board[0] = "X"
+  elsif turn_count(board)%2 != 0
+    board[turn_count(board)]="O"
+  elsif turn_count(board)%2 == 0
+    board[turn_count(board)]="X"
   end
 end
